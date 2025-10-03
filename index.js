670b4e5c521ce5dae082267405b85fea359e90f9
Punam <punamkumari399@gmail.com>
2025-10-03 21:49:13 +0530

    frontend url added

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ connectToMongo();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors("*"));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ims-inventory-management-system-five.vercel.app/",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", userRouter);
